459fea080cbfd635afd68e972c5a11cf796bdafe
vikas <v8kgsm@gmail.com>
2021-04-28 17:17:23 +0530

    wrapper function

---
+++ 02module/wrapper.js
@@ -0,0 +1,7 @@
+// after compiling the code is converted to a function
+// something similar like this
+// function((exports, require, module, __filename, __dirname)){}
+
+console.log(__filename);
+console.log(__dirname);
+console.log("wrapper");
