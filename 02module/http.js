45240ad91b3d3911bb0c055377962811f2c9a0d1
vikas <v8kgsm@gmail.com>
2021-04-29 10:23:12 +0530

    https

---
+++ 02module/http.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Server is active");
+    res.end();
+  }
+  if (req.url === "/courses") {
+    res.write(JSON.stringify([1, 2, 3, 4, 5]));
+    res.end();
+  }
+});
+
+// server.on("connection", (socket) => {
+//   console.log("New Connection");
+// });
+
+server.listen(5555);
+console.log("Listening on port 5555..");
