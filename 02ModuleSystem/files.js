ccdf30347d65bb1156fe8b74e33bf715491e778a
vikas <v8kgsm@gmail.com>
2021-12-22 09:14:11 +0530

    Files

--- 02ModuleSystem/files.js
+++ 02ModuleSystem/files.js
@@ -3,7 +3,9 @@ const fs = require("fs");
 const files = fs.readdirSync("./");
 console.log(files);
 
-// each function is available in sync and async
+// Each function is available in sync and async
+
+// In call function which is passed as an argument their are two arguments error and files(results) where at a time only one argument holds the value and the other one is false.
 
 fs.readdir("./", (err, files) => {
   err ? console.log(err) : console.log("Result", files);
