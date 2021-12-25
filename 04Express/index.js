9b065c0ad4012a8f9f18bb46ae357503c384e5c6
vikas <v8kgsm@gmail.com>
2021-12-25 20:28:59 +0530

    express

--- 04Express/index.js
+++ 04Express/index.js
@@ -1,5 +1,6 @@
 const Joi = require("joi");
 const express = require("express");
+// const { func } = require("joi");
 const app = express();
 
 app.use(express.json());
@@ -10,6 +11,14 @@ const courses = [
   { id: 3, name: "course3" },
   { id: 4, name: "course4" },
 ];
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(course);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello Express");
@@ -24,27 +33,44 @@ app.get("/api/courses/:id", (req, res) => {
   //   res.send(req.params);
   //   console.log(req.params);
   const course = courses.find((c) => c.id === parseInt(req.params.id));
+
   if (!course)
-    res.status(404).send("The course with the given ID was not found");
+    return res
+      .status(404)
+      .send(`The course with the Id: ${req.params.id} was not found`);
+
   res.send(course);
 });
 
 // Post request
 app.post("/api/courses", (req, res) => {
-  //   validation with Joi package
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
+  // we clean the code by creating function validation and written new code
+  const { error } = validateCourse(req.body);
 
-  const result = schema.validate(req.body.name, schema);
-  console.log(result);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  //   validation with Joi package
+  //   const schema = Joi.object({
+  //     name: Joi.string().min(3).required(),
+  //   });
+
+  //   const result = schema.validate(req.body);
+  //   console.log(result);
+
+  //   if (result.error) {
+  //     res.status(400).send(result.error.details[0].message);
+  //     return;
+  //     }
 
   // Manual validation
-  if (!req.body.name || req.body.name.length < 3) {
-    res
-      .status(400)
-      .send("Name is required and should be minimum 3 characters.");
-  }
+  //   if (!req.body.name || req.body.name.length < 3) {
+  //     res
+  //       .status(400)
+  //       .send("Name is required and should be minimum 3 characters.");
+  //     return;
+  //   }
 
   // request handler
   const course = {
@@ -52,6 +78,66 @@ app.post("/api/courses", (req, res) => {
     name: req.body.name,
   };
   courses.push(course);
+  res.send(course);
+});
+
+// Put request
+app.put("/api/courses/:id", (req, res) => {
+  // look up the course
+  // if not exisiting, return 404
+
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    return res
+      .status(404)
+      .send(`The course with the Id: ${req.params.id} was not found`);
+  }
+  // validate
+  // if invalid, return 404 - bad request
+
+  // we clean the code by creating function validation and written new code
+  const { error } = validateCourse(req.body);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  // old way
+
+  //   const schema = Joi.object({
+  //     name: Joi.string().min(3).required(),
+  //   });
+
+  //   const result = schema.validate(req.body);
+
+  //   if (result.error) {
+  //     res.status(400).send(result.error.details[0].message);
+  //     return;
+  //   }
+
+  // update course
+  // return the updated course
+  console.log(req.body.name);
+  course.name = req.body.name;
+  console.log(course.name);
+
+  res.send(course);
+});
+
+// deleting the course
+app.delete("/api/courses/:id", (req, res) => {
+  // validation
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res
+      .status(404)
+      .send(`The course with the Id: ${req.params.id} was not found`);
+
+  // delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  // return
   res.send(course);
 });
 
